e185026a89d0737c2193b5196b8c4facf3400b56
youngjun <whdudwns7100@naver.com>
2025-01-23 12:07:46 +0900

    fist commit

---
+++ src/test/java/com/example/studyroom/StudyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
